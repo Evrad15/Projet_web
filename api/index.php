46a65cc55a36a981d68c840801c2940fb0ea9cbb
Evrad <mopaoaristote15@gmail.com>
2026-03-06 00:36:24 +0100

    Configure Laravel document root and load index.php
    Set the document root to Laravel's public directory and load Laravel.

--- api/index.php
+++ api/index.php
@@ -1,3 +1,7 @@
 <?php
-// On redirige tout vers le dossier public de Laravel
+
+// Définir le chemin public de Laravel
+$_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/../public';
+
+// Charger Laravel
 require __DIR__ . '/../public/index.php';
