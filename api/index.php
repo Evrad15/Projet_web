8ebe498499eeaad0447b22d2ee8ca4e84a3d9209
Evrad <mopaoaristote15@gmail.com>
2026-03-06 01:51:46 +0100

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,14 +1,18 @@
 <?php
 
-// Utiliser /tmp pour le storage
-$_ENV['APP_STORAGE'] = '/tmp';
-putenv('APP_STORAGE=/tmp');
-
-// Créer les dossiers nécessaires dans /tmp
+// Créer les dossiers dans /tmp
 @mkdir('/tmp/storage/framework/views', 0777, true);
 @mkdir('/tmp/storage/framework/cache/data', 0777, true);
 @mkdir('/tmp/storage/framework/sessions', 0777, true);
+@mkdir('/tmp/storage/framework/sessions', 0777, true);
 @mkdir('/tmp/storage/logs', 0777, true);
+
+// Définir les variables d'environnement
+putenv('APP_STORAGE=/tmp/storage');
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+
+// Redéfinir les constantes de Laravel
+define('LARAVEL_STORAGE_PATH', '/tmp/storage');
 
 $_SERVER['DOCUMENT_ROOT'] = __DIR__ . '/../public';
 
