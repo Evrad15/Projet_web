aebc97cd0fe95e36c26d44cd1ea5750768d37138
Evrad <mopaoaristote15@gmail.com>
2026-03-08 14:13:45 +0100

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,8 +5,6 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
-use Faker\Factory as Faker; // <--- On importe la classe Faker manuellement
-
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
@@ -23,18 +21,15 @@ class UserFactory extends Factory
      * @return array<string, mixed>
      */
     public function definition(): array
-    {
-        // On crée l'instance de faker manuellement pour éviter le bug du "null"
-        $faker = Faker::create(); 
-
-        return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ];
-    }
+{
+    return [
+        'name' => $this->faker->name(),
+        'email' => $this->faker->unique()->safeEmail(),
+        'email_verified_at' => now(),
+        'password' => static::$password ??= Hash::make('password'),
+        'remember_token' => \Illuminate\Support\Str::random(10),
+    ];
+}
 
     /**
      * Indicate that the model's email address should be unverified.
