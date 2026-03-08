24785b7a376752a17af6c91387ce5b5139f6878a
Evrad <mopaoaristote15@gmail.com>
2026-03-08 13:49:54 +0100

    Update UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker; // <--- On importe la classe Faker manuellement
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,9 +24,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // On crée l'instance de faker manuellement pour éviter le bug du "null"
+        $faker = Faker::create(); 
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => $faker->name(),
+            'email' => $faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
