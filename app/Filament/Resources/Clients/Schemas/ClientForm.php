e3042c137148b533a8f62a420dffe5350a067148
Evrad <mopaoaristote15@gmail.com>
2026-03-08 18:52:02 +0100

    Update ClientForm.php

--- app/Filament/Resources/Clients/Schemas/ClientForm.php
+++ app/Filament/Resources/Clients/Schemas/ClientForm.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Filament\Resources\Clients\Schemas;
-
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
@@ -14,21 +12,25 @@ class ClientForm
                 TextInput::make('name')
                     ->label('Nom')
                     ->required(),
-
                 TextInput::make('email')
                     ->label('E-mail')
                     ->email()
                     ->required()
                     ->unique(table: 'clients', column: 'email', ignoreRecord: true),
-
                 TextInput::make('phone')
                     ->label('Téléphone')
                     ->tel()
                     ->required(),
-
                 TextInput::make('address')
                     ->label('Adresse')
                     ->required(),
+                TextInput::make('password')
+                    ->label('Mot de passe')
+                    ->password()
+                    ->revealable()
+                    ->required()
+                    ->minLength(8)
+                    ->dehydrated(false), // ne pas sauvegarder dans la table clients
             ]);
     }
 }
